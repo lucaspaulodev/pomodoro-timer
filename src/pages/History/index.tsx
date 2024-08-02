32f7404ca4b87d5e2fa22f8c43b5339d32f5bb10
Lucas Paulo <lucaspaulodev@gmail.com>
2024-08-02 03:47:20 -0300

    Styling History page.

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,3 +1,57 @@
+import { HistoryContainer, HistoryList, Status } from './styles'
+
 export function History() {
-  return <h1>History</h1>
+  return (
+    <HistoryContainer>
+      <h1>My History</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Task</th>
+              <th>Duration</th>
+              <th>Start</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>Task</td>
+              <td>20 min</td>
+              <td>2 months ago</td>
+              <td>
+                <Status statusColor="yellow">Done</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Task</td>
+              <td>20 min</td>
+              <td>2 months ago</td>
+              <td>
+                <Status statusColor="green">Done</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Task</td>
+              <td>20 min</td>
+              <td>2 months ago</td>
+              <td>
+                <Status statusColor="red">Done</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Task</td>
+              <td>20 min</td>
+              <td>2 months ago</td>
+              <td>
+                <Status statusColor="yellow">Done</Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
 }
